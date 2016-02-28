38decbc0b501b9e0b6bf4fcc08921b23d69319a8
practicingprogramming <practicingprogramming@gmail.com>
2016-02-27 18:04:03 -0800

    Make TestSuiteRunResult observable

--- lib/clapt/test_suite_run_result.rb
+++ lib/clapt/test_suite_run_result.rb
@@ -1,6 +1,10 @@
+require 'observer'
+
 module Clapt
   # Test Suite Run Result.
   class TestSuiteRunResult
+    include Observable
+
     attr_reader :test_run_results
 
     def initialize
@@ -9,6 +13,8 @@ module Clapt
 
     def add(test_run_result)
       @test_run_results << test_run_result
+      changed
+      notify_observers(test_run_results)
     end
   end
 end
--- spec/test_suite_run_result_spec.rb
+++ spec/test_suite_run_result_spec.rb
@@ -2,11 +2,18 @@ describe Clapt::TestSuiteRunResult do
   describe '#add' do
     before do
       @test_suite_run_result = Clapt::TestSuiteRunResult.new
+      allow(@test_suite_run_result).to receive(:notify_observers)
+      allow(@test_suite_run_result).to receive(:changed)
       @test_suite_run_result.add(Clapt::TestRunResult.new(nil, true))
     end
 
     it 'adds result to the list' do
       expect(@test_suite_run_result.test_run_results.length).to eq(1)
+    end
+
+    it 'notifies observers' do
+      expect(@test_suite_run_result).to have_received(:changed)
+      expect(@test_suite_run_result).to have_received(:notify_observers)
     end
   end
 end
