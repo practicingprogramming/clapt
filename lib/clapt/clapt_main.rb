9737f57d90e568270e7a09d7c447afec5dd175d2
practicingprogramming <practicingprogramming@gmail.com>
2016-02-27 18:24:59 -0800

    Call everything from main

--- lib/clapt/clapt_main.rb
+++ lib/clapt/clapt_main.rb
@@ -4,11 +4,16 @@ module Clapt
     def initialize(args)
       @test_discoverer = args[:test_discoverer]
       @directory = args[:directory]
+      @test_runner = args[:test_runner]
+      @result_writer = args[:result_writer]
     end
 
     def run
-      @test_registry = @test_discoverer.discover(@directory)
+      test_registry = @test_discoverer.discover(@directory)
       puts "Tests: #{TestRegistry.instance.tests}"
+      test_suite_run_result = TestSuiteRunResult.new
+      test_suite_run_result.add_observer(@result_writer)
+      @test_runner.run(test_registry, test_suite_run_result)
     end
   end
 end
--- spec/clapt_main_spec.rb
+++ spec/clapt_main_spec.rb
@@ -1,17 +1,28 @@
 describe Clapt::ClaptMain do
   describe 'run' do
     before do
-      @test_discoverer = double
+      @test_discoverer = double(Clapt::TestDiscoverer)
       allow(@test_discoverer).to receive(:discover).and_return('discovered tests')
+      @test_runner = double(Clapt::TestRunner)
+      allow(@test_runner).to receive(:run)
+      @result_writer = double(Clapt::ResultWriter)
+      allow(@result_writer).to receive(:update)
       @clapt_main = Clapt::ClaptMain.new(
         test_discoverer: @test_discoverer,
-        directory: 'test_dir'
+        directory: 'test_dir',
+        result_writer: Clapt::ResultWriter.new,
+        test_runner: @test_runner,
+        result_writer: @result_writer
       )
       @clapt_main.run
     end
 
     it 'discovers tests' do
       expect(@test_discoverer).to have_received(:discover).with('test_dir')
+    end
+
+    it 'runs tests' do
+      expect(@test_runner).to have_received(:run)
     end
   end
 end
