fa6f253f6d0da01721b7739ea1ef618293df9dca
practicingprogramming <practicingprogramming@gmail.com>
2016-02-28 17:46:41 -0800

    Implement Test

--- lib/clapt/test.rb
+++ lib/clapt/test.rb
@@ -1,5 +1,8 @@
+require 'pty'
+
 module Clapt
   # Test case.
+  # TODO: comment, unit test.
   class Test
     attr_reader :name
 
@@ -11,6 +14,35 @@ module Clapt
 
     def run
       @block.call(self)
+    end
+
+    def start(command)
+      @stdout, @stdin, @pid = PTY.spawn(command)
+    end
+
+    def input(value)
+      @stdin.puts value
+      @last_input = value
+    end
+
+    # rubocop:disable HandleExceptions
+    def output(expected)
+      result = ''
+      begin
+        Timeout.timeout(1) do
+          @stdout.each do |line|
+            result += line if line.chomp != @last_input
+          end
+        end
+      rescue Timeout::Error
+      end
+      fail "Output #{result} doesn't match expected #{expected}" unless result.match(expected)
+    end
+    # rubocop:enable HandleExceptions
+
+    def terminate
+      @stdin.close
+      @stdout.close
     end
   end
 end
